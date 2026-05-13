e374803b23743322d8896bfe9fd9b723574245c3
sambweber <42612830+sambweber@users.noreply.github.com>
2026-05-13 11:34:21 +0100

    Update PSAT_Tools.R

--- R/PSAT_Tools.R
+++ R/PSAT_Tools.R
@@ -149,7 +149,7 @@ gpe_residency <- function(gpe3_files, track_end = NULL, weight=F,percentile=0.85
   
   output = lapply(output,FUN = function(x){extend(x,boundaries,value=0)})
   
-  population_average = overlay(do.call("stack",output),fun=mean) 
+  population_average = raster::overlay(do.call("stack",output),fun=mean) 
   
   return(volume_contour(population_average, ...))
   
